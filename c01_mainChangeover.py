d9cfaac967f7a10b53d742df1a39f3761cb09ea3
Nico Koehler <nico.koehler@danfoss.com>
2019-07-18 14:35:35 +0200

    Fixed commenting and removed obnsolete statements;

--- c01_mainChangeover.py
+++ c01_mainChangeover.py
@@ -1,5 +1,18 @@
 # --------CHANGE OVERS
 
+######################################### 0 Intro ######################################### 
+# Naming Convention: first letter of variable indicates the type
+# a = array
+# b = binary / boolean
+# c = code, for .py files only
+# d = dictionary
+# f = float
+# i = integer
+# l = list
+# lim = limit
+# s = string
+# file = file generated or modified through code
+
 import pyodbc as db
 import random
 import pandas as pd
@@ -10,25 +23,6 @@ import sys
 import matplotlib.pyplot as plt
 import math
 import datetime
-
-
-		
-
-######################################### 0 GLOBAL  VARIABLES ######################################### 
-
-
-print(glob.iCataclysmicProb)
-
-# fMutationRate = 0.2
-# glob.limPopulationSize = 10	# has to be multiple of 2
-# glob.iChildCounter = glob.limPopulationSize
-# glob.iGenerationCount = 0
-# glob.iBreakGeneration = 5
-# glob.iPastAverage = 5
-# glob.iNumberMachines = 4
-# glob.iDeletionProb = 0.25
-# glob.iCataclysmicProb = 0.50
-# glob.iBreakImport = 10
 
 
 fMinFitness = 100000000
@@ -69,7 +63,6 @@ dfMachineConfig = pd.read_excel("C:\\Users\\u374441\\Desktop\\desktopWorkfiles\\
 dfMaterialCO = pd.read_excel("C:\\Users\\u374441\\Desktop\\desktopWorkfiles\\201905 Changeovers\\03_co_setup_alternative.xlsx", sheet_name="materialCO")
 
 #fill WC List
-
 for index, row in dfWCImport.iterrows():
 
 	if index >= glob.iBreakImport:
@@ -92,29 +85,24 @@ for index, row in dfFamilies.iterrows():
 	dMaterialFamily[row.Material] = {}
 	dMaterialFamily[row.Material]['family'] = row.materialFamily
 	dMaterialFamily[row.Material]['cycleTime'] = row.cycleTime
-
+#Create MachineConfig 
 for index, row in dfMachineConfig.iterrows():
 	dMachineConfig[row.family] = [int(x) for x in str(row.notOnMachine).split(",")]
 
-
+#Create Material changeover time mapping
 for index, row in dfMaterialCO.iterrows():
 	dMaterialCO[row.materialRel] = row["timeCO"]
 	glob.lMaterialAtlas_0.append(row["materialAtlas"])
 
 
-
-
 #open file to track usage history
 filePopulationHistory = open("C:\\Users\\u374441\\Desktop\\desktopWorkfiles\\201905 Changeovers\\90_populationHistory.txt", "w", encoding="utf-8")
-
 fileFitnessHistory_runs = open("C:\\Users\\u374441\\Desktop\\desktopWorkfiles\\201905 Changeovers\\91_fitnessRuns.txt", "a", encoding="utf-8")
 
 ######################################### 2 GA SETUP ######################################### 
 # TO DO
 # > use a more intelligent filling for initial population 
-
 ### 2.1 Iterate over WC list and populate arrays
-
 # initialize population randomly from list
 lGenome = []
 lPopulation = []
@@ -122,232 +110,184 @@ dPopulation = {}
 lPopulation_names =[]
 glob.lGenome_0 = []
 
-
+# create original genome with all orders contained
 for order in dWcList.keys():
 	glob.lGenome_0.append(order)
 
+# create list of 0s to fill fill the genome to a length of machines x genes to represent all possible machines in one genome
 lEmptyAppend = [i*0 for i in range(0, (glob.iNumberMachines-1)*len(glob.lGenome_0))]
-
 lGenome = glob.lGenome_0+lEmptyAppend
 
 
-
+# from the filled Genome, create n = limPopulationSize initial parents
 for i in range(0,glob.limPopulationSize):
 	lPopulation.append([])
 	lGenesAvailable = lGenome.copy()
-	#print(i)
+
 	lPopulation_names.append("member"+str(i))
 	lBreakGenome = []
 	lCOlookup = []
 
-	#fill member randomly based on available genes - reduce available genes
+	# fill member randomly based on available genes - reduce available genes
 	for gene in range(0, len(lGenome)):
 		sOrder= random.choice(lGenesAvailable)
 		lPopulation[i].append(sOrder)
 		lGenesAvailable.remove(sOrder)
 
-
+	# craete breaking points / machine seperators based on the lenght of a single machine (length of the origin genome)
 	for j in range(1, glob.iNumberMachines):
-		
 		iRandBreaker = (j*len(glob.lGenome_0))
-			#print("iBreaker in List, restarting")
-
 		lBreakGenome.append(iRandBreaker)
 
+	# legacy sort from when the breaker was generated randomly
 	lBreakGenome.sort()
 
-	#print(lCOlookup)
-	#print(gak.udf_listSortByBreak(lCOlookup, lBreakGenome, 0))
-
-
-
+	# populate the Population dictionary
 	dPopulation["member"+str(i)] = {}
 	dPopulation["member"+str(i)]["genome"] = gak.udf_listSortByBreak(lPopulation[i], lBreakGenome, 0)
 	dPopulation["member"+str(i)]["breaker"] = lBreakGenome	
 
-
-
-
-#for i,j in dPopulation.items():
-#	gak.udf_printMachinesCMD(j["genome"], j["breaker"], i)
-
-
+# write the first population to the history file
 filePopulationHistory.write("#"+str(glob.iGenerationCount)+".1------------------------ Original Population ------------------------"+"\n")
 for i,w in enumerate(lPopulation):
 	filePopulationHistory.write(lPopulation_names[i]+": "+str(w)+"\n")
 
 
-#print(lPopulation)
-#print("---------------------")
-#print(dPopulation)
-
-### 2.2 Estimate fitness of each member
-
-
 
 ######################################### 3 GA Algorithm ######################################### 
-#! Arrays ending on "_names" are parallel arrays to track member names
-#iterate until break point reached (see below)
+# ! Arrays ending on "_names" are parallel arrays to track member names
+# iterate until break point reached (see below)
 while True:
 
 	glob.iGenerationCount += 1
 	print("--------------------------------- GENERATION: "+str(glob.iGenerationCount)+"---------------------------------")
 
 
-	#execute function to calculate fitness of population
-
-	########## THE CATACLYSM ##########
+	# execute function to calculate fitness of population
+	# determine randomly if a cataclysm should occur; cataclsym = "kills off" the population and fills it with newly created one
 	if random.uniform(0.0, 1.0) < glob.iCataclysmicProb and glob.bCataclysm == True:
 
 		print("<<<<<<<<<<<<<<<<<<< CATACLYSM TIME <<<<<<<<<<<<<<<<<<<")
 
-		#for i,j in dPopulation.items():
-		#	print(i,j)
-	
 		dPopulation = gak.udf_cataclysm(dPopulation, glob.lGenome_0)
-		glob.iBreakGeneration +=100
+		# Add runs to the overall counter after cataclysm
+		glob.iBreakGeneration += 100
 
-	#print("-------------- New post apocalyptic society --------------")
-	fIllegalPerc=0.0
-
+	# calculte fitness for each member in the population
+	fIllegalPerc = 0.0
 	lFitness, dMembers, lMinFitness, fMinFitness_run, fIllegalPerc = gak.udf_calcFitness3(lPopulation, dPopulation, dWcList, dMaterialFamily, dFamilyCO, dMaterialCO, lMinFitness, lPopulation_names, glob.iNumberMachines, dMachineConfig)
-
 	lFitness_history.append(fMinFitness_run)
 	lIllegal_history.append(fIllegalPerc)
 
-
-	#print(fMinFitness)
-
-	#print(lFitness)
-	#print(dMembers)
-	#print(lMinFitness)
-
-
-	#if the fitness is lower then the previous fintness lever, update the minimum fitness
+	# if the fitness is lower then the previous fintness lever, update the minimum fitness
 	if lMinFitness[0] <= fMinFitness:
 		fMinFitness = lMinFitness[0]
 
-	#append calculated fitness for new lowest level
+	# append calculated fitness for new lowest level
 	lMinFitness_history.append(fMinFitness)
 
-	#create table and calculate selection probabilities
+	# create table and calculate selection probabilities
 	lFitness_sorted = gak.udf_sortByFitness(lFitness)
 
 
-	# for i,j in dMembers.items():
-	# 	print(i, j["fitness"], j["illegal"])
-
-
-
-	#initialize population arrays
+	# initialize population arrays
 	lPopulation_new = []
 	lPopulation_new_names = []
 	dPopulation_new ={}
 
-	#select parents randomly to form new population
+	# select parents randomly to form new population
 	lPopulation_new, lPopulation_new_names, dPopulation_new = gak.udf_selectParentsFromPool(dMembers, glob.limPopulationSize, lFitness_sorted, dPopulation)
 
 
-	#Mating time - execute mating functions and initialize offspring arrays
+	# Mating time - execute mating functions and initialize offspring arrays
 	lPopulation_offspring = []
 	lPopulation_offspring_names = []
 	dPopulation_offspring ={}
 
-	#lPopulation_offspring, glob.iChildCounter, lPopulation_offspring_names, dPopulation_offspring = gak.udf_matingPMX(lPopulation_new, glob.iChildCounter, lPopulation_new_names, dPopulation_new, dMembers, glob.fMutationRate)
+	# lPopulation_offspring, glob.iChildCounter, lPopulation_offspring_names, dPopulation_offspring = gak.udf_matingPMX(lPopulation_new, glob.iChildCounter, lPopulation_new_names, dPopulation_new, dMembers, glob.fMutationRate)
 	lPopulation_offspring, lPopulation_offspring_names, dPopulation_offspring = gak.udf_cloneMutate(lPopulation_new, lPopulation_new_names, dPopulation_new, dMembers, dMaterialFamily, dMachineConfig, dWcList, lGenome)
 
 
-	#Mutating Time - execute swap-mutate function
+	# Mutating Time - execute swap-mutate function
 	lPopulation_offspringMutated = []
 	lPopulation_offspringMutated, dPopulation_offspring = gak.udf_mutateSwap(glob.fMutationRate,lPopulation_offspring, dPopulation_offspring)
 	
 
-	#overwrite previous population with new selected and offspring
+	# overwrite previous population with new selected and offspring
 	lPopulation = lPopulation_new + lPopulation_offspringMutated
 	lPopulation_names = lPopulation_new_names + lPopulation_offspring_names
-	#dPopulation = {**dPopulation_new, **dPopulation_offspring}
 
+	# recreate the population array with the selected parents from previous iteration
 	dPopulation={}
 	for i,member in dPopulation_new.items():
-
-
+		# avoid double entries, which are technically possible due to selection method
 		if member["member"] not in dPopulation:
 			dPopulation[member["member"]]={}
 			dPopulation[member["member"]]["genome"]=member["genome"]
 			dPopulation[member["member"]]["breaker"]=member["breaker"]
 
-
-
-
-
-
+	# deconstruct newly created parent array and the mutated offspring array
 	dPopulation = {**dPopulation, **dPopulation_offspring}
-		#for i, j in dPopulation.items():
-	#	print(i, j)
-	# if lMinFitness[1] in dPopulation:
 
-	# 	print("#E7 PREF CHILD: ", lMinFitness[1], dPopulation[lMinFitness[1]]["genome"])
-	
-
-
-	#break the while loop if generation limit is reached
-	if glob.iGenerationCount == glob.iBreakGeneration:
-		break
-
-
-	#calculate starting point for trailing average
+	# calculate starting point for trailing average
 	iAvgStart = len(lMinFitness_history)-glob.iPastAverage
 	if iAvgStart < 5:
 		iAvgStart = 0
 
-
-	#break the while loop if no lower fitness could be found for "iAvgStart" number of generations
+	# break the while loop if no lower fitness could be found for "iAvgStart" number of generations
 	if sum(lMinFitness_history[(iAvgStart):(len(lMinFitness_history))]) < fMinFitness:
 		break
 
-#	for i,j in dPopulation.items():
-#		print(i)
-			
-#close file
+	# break the while loop if generation limit is reached
+	if glob.iGenerationCount == glob.iBreakGeneration:
+		break
+
+
+# close file
 filePopulationHistory.close()
+
+# terminal end prints
 print("===============================================================================================")
 print("RESULT: ", lMinFitness[0])
 print("MEMBER: ", lMinFitness[1])
 print( lMinFitness[4])
 
-#print(lFitness_history)
-#print(lFitness_history)
-#print(lIllegal_history)
-
 print(np.corrcoef(lFitness_history, lIllegal_history)[1])
 
+# print machines in termial
 gak.udf_printMachinesCMD(lMinFitness[2], lMinFitness[3], lMinFitness[1])
 print("__________________________________________")
-gak.udf_printMachinesFamCMD(lMinFitness[2], lMinFitness[3], lMinFitness[1], dMaterialFamily, dWcList)
-############################### GRAPH IT
 
+# print machines with familes not materials
+gak.udf_printMachinesFamCMD(lMinFitness[2], lMinFitness[3], lMinFitness[1], dMaterialFamily, dWcList)
+
+
+######################################### 4 Graphing it #########################################
+# set min and max for the y axes
 y1Min = math.floor(min(lFitness_history)/1000)*1000
 y1Max = math.ceil(min(lFitness_history)/1000)*1100
-
 y2Min = math.floor(min(lIllegal_history))-0.1
 y2Max = math.ceil(min(lIllegal_history))+0.1
 
-
+# set parameters for saving the plot
 dt = datetime.datetime.now()
 iMilliseconds = int(round(dt.timestamp() * 1000))
 
 sFileNamePl0t = str(iMilliseconds)+"__RESULT_"+str(math.floor(lMinFitness[0]))+"__orders_"+str(len(glob.lGenome_0))+"--machines_"+str(glob.iNumberMachines)+"--Runs_"+str(glob.iBreakGeneration)+"--popSize_"+str(glob.limPopulationSize)+"--mut_"+str(glob.fMutationRate)+"--King_"+str(glob.bKingPrevails)+"--fAlloc_"+str(glob.iForceAllocation_G)+"--CAT_"+str(glob.bCataclysm)+"_"+str(glob.iCataclysmicProb)+"_"+str(glob.iDeletionProb)
 sPlotPath = "C:\\Users\\u374441\\Desktop\\desktopWorkfiles\\201905 Changeovers\\99_Output\\"+sFileNamePl0t+".png"
 
+# create subplot
 fig, ax1 = plt.subplots()
 
+# set options
 color = "tab:blue"
 ax1.set_ylabel("Fitness")
 ax1.set_xlabel("Runs")
 ax1.set_ylim(y1Min, y1Max)
 ax1.plot(lFitness_history, color=color)
 
-ax2=plt.twinx()
+# create twin axis and set 
+ax2 = plt.twinx()
 color ="tab:green"
 ax2.set_ylabel("Illegal Percentage")
 ax2.set_ylim(y2Min, y2Max)
@@ -355,10 +295,9 @@ ax2.plot(lIllegal_history, color=color, linestyle="--")
 
 fig.tight_layout()
 
-
+#save and plot
 plt.savefig(sPlotPath)
 plt.show()
-
 
 fileFitnessHistory_runs.write(str(lMinFitness[0])+"\n")
 
